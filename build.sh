d2decf6023a829de5f537757342836e1dfd6b4e9
Rasmey SARETH <rasmey.sareth@gmail.com>
2018-10-10 10:57:02 +0200

    Implemented the function to build everything

--- build.sh
+++ build.sh
@@ -2,7 +2,19 @@
 
 set -e
 
+__build_everything() {
+  echo "Building everything ..."
+  for i in $(find . -maxdepth 2 -name "package.json")
+  do
+    CURRENT_DIRECTORY_NAME=$(dirname $i | sed -re "s@\.\/@@g")
+    cd "${CURRENT_DIRECTORY_NAME}"
+    echo "---> ${CURRENT_DIRECTORY_NAME}"
+    cd - > /dev/null
+  done
+}
+
 __build_updated_directories() {
+  echo "Building the updated directories ..."
   LIST=$(git diff --name-status master..HEAD | \
           sed -re 's/^[A-Z]//g' -e 's/^[[:space:]]*//' | \
           cut -d/ -f1)
@@ -21,8 +33,7 @@ __build_updated_directories() {
 CURRENT_BRANCH_NAME=$(git branch | cut -d" " -f2)
 if [ "${CURRENT_BRANCH_NAME}" = "master" ]
 then
-  echo "Build everything"
+  __build_everything
 else
-  echo "Building the updated directories"
   __build_updated_directories
 fi
